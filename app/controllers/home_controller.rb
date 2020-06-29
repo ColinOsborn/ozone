6d86775eb1029b4299ba62268f81b5f8f3b9a500
Colin Osborn <colin.osborn@gmail.com>
2020-06-28 21:13:14 -0600

    very basic output reading from api to gather data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,13 @@
 class HomeController < ApplicationController
 
+    def index 
+        require 'net/http'
+        require 'json'
+        # @url = 'http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=81611&distance=2&API_KEY=B13144EB-18EB-4067-8485-1473BF537575'
+        @url = "http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=81623&distance=100&API_KEY=B13144EB-18EB-4067-8485-1473BF537575"
+        @uri = URI(@url)
+        @response = Net::HTTP.get(@uri)
+        @output = JSON.parse(@response)
+    end
+
 end
